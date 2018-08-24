2cdae0c160d2e509212218f267c03def3a699f27
smalldog124 <golf.apipol@gmail.com>
2018-08-24 17:30:56 +0700

    [Edited]: แยก Config ไว้ใน configs/development.json

--- src/gow-backend/main/main.go
+++ src/gow-backend/main/main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	apiLibrary "gow-backend/api"
 	"gow-backend/repository"
 	"gow-backend/route"
 	"gow-backend/service"
+	"io/ioutil"
+	"os"
 )
 
+type Config struct {
+	Mysql string `json:"mysql"`
+	Port  string `json:"port"`
+}
+
 func main() {
-	db, err := sql.Open("mysql", "root:sckshuhari@/GOW?parseTime=true")
+	var config Config
+	environment := "development"
+	if os.Getenv("ENV") != "" {
+		environment = os.Getenv("ENV")
+	}
+
+	configFile, err := ioutil.ReadFile(fmt.Sprintf("./configs/%s.json", environment))
+	if err != nil {
+		fmt.Printf("cannot read config file %s", err)
+		return
+	}
+
+	err = json.Unmarshal(configFile, &config)
+	if err != nil {
+		fmt.Printf("cannot Unmarshal config %s", err)
+		return
+	}
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s?parseTime=true", config.Mysql))
 	if err != nil {
 		panic(err.Error())
 	}
@@ -24,5 +51,5 @@ func main() {
 		CustomerService: &customerService,
 	}
 	route := route.NewRoute(api)
-	route.Run(":3000")
+	route.Run(fmt.Sprintf(":%s", config.Port))
 }
