eab0ecc1e277d9599f08801f51400c2cd48e44a5
platylm <n_nareenart@kkumail.com>
2018-08-27 11:36:30 +0700

    [Created]:interface companiesService and model company

--- src/gow-backend/service/service.go
+++ src/gow-backend/service/service.go
@@ -8,3 +8,8 @@ type CustomerService interface {
 	CreateNewCustomer(model.NewCustomer) (model.CustomerInfo, error)
 	ListCustomers() ([]model.CustomerInfo, error)
 }
+
+type CompaniesService interface {
+	CreateNewCompany(model.NewCompany) (model.CompanyInfo, error)
+	GetCompanies() ([]model.CompanyInfo, error)
+}
