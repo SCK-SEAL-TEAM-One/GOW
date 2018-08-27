68f0ce600135cef455354807c0cea455d6ab622d
platylm <n_nareenart@kkumail.com>
2018-08-27 15:01:40 +0700

    [Added]:function insert company

--- src/gow-backend/gow/repository/company.go
+++ src/gow-backend/gow/repository/company.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	"gow/model"
+	"time"
 )
 
 type CompanyRepository interface {
@@ -11,4 +12,20 @@ type CompanyRepository interface {
 }
 type CompanyRepositoryMySQL struct {
 	DBConnection *sql.DB
+}
+
+func (comrepo CompanyRepositoryMySQL) Insert(newCompany model.NewCompany) (bool, error) {
+	statementInsert, err := comrepo.DBConnection.Prepare(`INSERT INTO company 
+	(company_name,company_branch,company_address,company_taxid,company_phonenumber,created_time,updated_time)
+	VALUES( ?, ?, ?, ?, ?, ?, ? )`)
+	if err != nil {
+		return false, err
+	}
+	defer statementInsert.Close()
+	_, err = statementInsert.Exec(newCompany.Company, newCompany.Branch, newCompany.Address, newCompany.TaxID, newCompany.PhoneNumber, time.Now(), time.Now())
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+
 }
