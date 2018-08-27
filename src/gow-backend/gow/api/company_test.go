06a5f32a789061b7eeb0a87eb9fc9d0542124b34
smalldog124 <golf.apipol@gmail.com>
2018-08-27 15:12:27 +0700

    [Edited]:สร้างฟังก์ชัน getAllcompanies

--- src/gow-backend/gow/api/company_test.go
+++ src/gow-backend/gow/api/company_test.go
@@ -30,8 +30,28 @@ func Test_CreateCompanyHandler_Input_Siamchamnankit_Should_Be_Status_201_And_Com
 
 	if response.StatusCode != http.StatusCreated {
 		t.Errorf("expect \n'%d' \nbut got it \n'%d'", http.StatusCreated, response.StatusCode)
-
 	}
+	if string(expected) != string(actual) {
+		t.Errorf("expect \n'%s' \nbut got it \n'%s'", expected, actual)
+	}
+}
+
+func Test_GetAllCompaniesHandler_Should_Be_CompanyInfo(t *testing.T) {
+	expected, _ := ioutil.ReadFile("companiesResponseInfo.json")
+
+	request := httptest.NewRequest("GET", "/api/v1/companies", nil)
+	writer := httptest.NewRecorder()
+	mockGetcompanies := mockCompanyService{}
+	api := CompanyAPI{
+		CompanyService: &mockGetcompanies,
+	}
+	testRoute := route.NewRoute(api, CustomerAPI{})
+
+	testRoute.ServeHTTP(writer, request)
+
+	response := writer.Result()
+	actual, _ := ioutil.ReadAll(response.Body)
+
 	if string(expected) != string(actual) {
 		t.Errorf("expect \n'%s' \nbut got it \n'%s'", expected, actual)
 	}
