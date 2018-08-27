181ef879d9c56e29f3d9cd9a9e35027ee43025af
Apipol Sukgler <apipol@computerlogy.com>
2018-08-27 10:25:25 +0700

    [Edited]: ย้าย mock ไปที่ไฟล์ mock_test.go และลบโค้ดที่ไม่ได้ใช้ใน list customer เนื่องจากไม่มี input ใน request

---
+++ src/gow-backend/api/mock_test.go
@@ -0,0 +1,30 @@
+package api_test
+
+import (
+	"encoding/json"
+	"gow-backend/model"
+	"io/ioutil"
+)
+
+type mockCustomerService struct {
+}
+
+func (mhs mockCustomerService) ListCustomers() ([]model.CustomerInfo, error) {
+	var customers = []model.CustomerInfo{
+		model.CustomerInfo{
+			ID:      1,
+			Company: "บริษัท ที.เอ็น. อินคอร์ปอเรชั่น จำกัด",
+			Branch:  "สำนักงานใหญ่",
+			Address: "3 อาคารรัจนาการ ถนนสาทรใต้ แขวงยานนาวา เขตสาทร กรุงเทพมหานคร 10120",
+			TaxID:   "0105553108372",
+		},
+	}
+
+	return customers, nil
+}
+func (mhs mockCustomerService) CreateNewCustomer(newcustomer model.NewCustomer) (model.CustomerInfo, error) {
+	var customerInfo model.CustomerInfo
+	customers, _ := ioutil.ReadFile("./customerResponse.json")
+	json.Unmarshal(customers, &customerInfo)
+	return customerInfo, nil
+}
