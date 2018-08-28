1f4dc551ae9bfd9d49ec6f7587ec35a10ad38474
Apipol Sukgler <apipol@computerlogy.com>
2018-08-28 15:44:44 +0700

    [Edited]: แก้ sql ใส่ ; หลัง คำสั่ง

--- src/gow-backend/sql/gow.sql
+++ src/gow-backend/sql/gow.sql
@@ -1,28 +1,28 @@
 CREATE DATABASE IF NOT EXISTS GOW;
 USE GOW;
 CREATE TABLE IF NOT EXISTS customer (
-customer_id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-customer_name_th VARCHAR(512),
-customer_name_en VARCHAR(512),
-customer_branch_th VARCHAR(256),
-customer_branch_en VARCHAR(256),
-customer_address_th VARCHAR(256),
-customer_address_en VARCHAR(256),
-customer_taxid VARCHAR(13) UNIQUE,
-created_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-updated_time DATETIME ON UPDATE CURRENT_TIMESTAMP
-)
+    customer_id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    customer_name_th VARCHAR(512),
+    customer_name_en VARCHAR(512),
+    customer_branch_th VARCHAR(256),
+    customer_branch_en VARCHAR(256),
+    customer_address_th VARCHAR(256),
+    customer_address_en VARCHAR(256),
+    customer_taxid VARCHAR(13) UNIQUE,
+    created_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_time DATETIME ON UPDATE CURRENT_TIMESTAMP
+);
 
 CREATE TABLE IF NOT EXISTS company (
-company_id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-company_name_th VARCHAR(512),
-company_name_en VARCHAR(512),
-company_branch_th VARCHAR(256),
-company_branch_en VARCHAR(256),
-company_address_th VARCHAR(256),
-company_address_en VARCHAR(256),
-company_taxid VARCHAR(13) UNIQUE,
-company_phonenumber VARCHAR(13),
-created_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-updated_time DATETIME ON UPDATE CURRENT_TIMESTAMP
-)
+    company_id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    company_name_th VARCHAR(512),
+    company_name_en VARCHAR(512),
+    company_branch_th VARCHAR(256),
+    company_branch_en VARCHAR(256),
+    company_address_th VARCHAR(256),
+    company_address_en VARCHAR(256),
+    company_taxid VARCHAR(13) UNIQUE,
+    company_phonenumber VARCHAR(13),
+    created_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_time DATETIME ON UPDATE CURRENT_TIMESTAMP
+);
