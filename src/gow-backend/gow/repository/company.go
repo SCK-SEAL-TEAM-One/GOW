cb7e31fbf71b23d18ea29b96a33a2d92b77cb181
Apipol Sukgler <apipol@computerlogy.com>
2018-08-28 14:50:03 +0700

    [Edied]:แก้ คำสั่ง sql ให้ตรง

--- src/gow-backend/gow/repository/company.go
+++ src/gow-backend/gow/repository/company.go
@@ -16,14 +16,17 @@ type CompanyRepositoryMySQL struct {
 }
 
 func (comrepo CompanyRepositoryMySQL) Insert(newCompany model.NewCompany) (bool, error) {
-	statementInsert, err := comrepo.DBConnection.Prepare(`INSERT INTO company 
-	(company_name,company_branch,company_address,company_taxid,company_phonenumber,created_time,updated_time)
-	VALUES( ?, ?, ?, ?, ?, ?, ? )`)
+	company := newCompany.ToCompanyModel()
+	statement := `INSERT INTO company 
+	(company_name_th,company_name_en,company_branch_th,company_branch_en,company_address_th,company_address_en,
+	company_taxid,company_phonenumber,created_time,updated_time)
+	VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )`
+	statementInsert, err := comrepo.DBConnection.Prepare(statement)
 	if err != nil {
 		return false, err
 	}
 	defer statementInsert.Close()
-	_, err = statementInsert.Exec(newCompany.Company, newCompany.Branch, newCompany.Address, newCompany.TaxID, newCompany.PhoneNumber, time.Now(), time.Now())
+	_, err = statementInsert.Exec(company.NameTH, company.NameEN, company.BranchTH, company.BranchEN, company.AddressTH, company.AddressEN, company.TaxID, company.PhoneNumber, time.Now(), time.Now())
 	if err != nil {
 		return false, err
 	}
@@ -32,23 +35,26 @@ func (comrepo CompanyRepositoryMySQL) Insert(newCompany model.NewCompany) (bool,
 }
 
 func (comrepo CompanyRepositoryMySQL) GetByTaxID(companyTaxID string) (model.CompanyInfo, error) {
-	var companyInfo model.CompanyInfo
+	var company model.Company
 	statementQuery := `SELECT * FROM company WHERE company_taxid=?`
 	row := comrepo.DBConnection.QueryRow(statementQuery, companyTaxID)
 	err := row.Scan(
-		&companyInfo.ID,
-		&companyInfo.Company,
-		&companyInfo.Branch,
-		&companyInfo.Address,
-		&companyInfo.TaxID,
-		&companyInfo.PhoneNumber,
-		&companyInfo.CreatedTime,
-		&companyInfo.UpdatedTime,
+		&company.ID,
+		&company.NameTH,
+		&company.NameEN,
+		&company.BranchTH,
+		&company.BranchEN,
+		&company.AddressTH,
+		&company.AddressEN,
+		&company.TaxID,
+		&company.PhoneNumber,
+		&company.CreatedTime,
+		&company.UpdatedTime,
 	)
 	if err != nil {
 		return model.CompanyInfo{}, err
 	}
-	return companyInfo, nil
+	return company.ToCompanyInfo(), nil
 }
 
 func (comrepo CompanyRepositoryMySQL) GetAll() ([]model.CompanyInfo, error) {
@@ -59,18 +65,21 @@ func (comrepo CompanyRepositoryMySQL) GetAll() ([]model.CompanyInfo, error) {
 		return companyInfo, err
 	}
 	for rows.Next() {
-		var company model.CompanyInfo
+		var company model.Company
 		rows.Scan(
 			&company.ID,
-			&company.Company,
-			&company.Branch,
-			&company.Address,
+			&company.NameTH,
+			&company.NameEN,
+			&company.BranchTH,
+			&company.BranchEN,
+			&company.AddressTH,
+			&company.AddressEN,
 			&company.TaxID,
 			&company.PhoneNumber,
 			&company.CreatedTime,
 			&company.UpdatedTime,
 		)
-		companyInfo = append(companyInfo, company)
+		companyInfo = append(companyInfo, company.ToCompanyInfo())
 	}
 	return companyInfo, nil
 }
