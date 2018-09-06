504fc806232fe3de90245e360b2643a4476bf071
Apipol Sukgler <apipol@computerlogy.com>
2018-09-06 11:43:00 +0700

    [Edited]: แก้ไข้ให้ configuration สามารถ override ผ่าน environment variable ได้, ลบไฟล์ binary ของ gow ที่ไม่ต้อง commit

--- src/gow-backend/gow/config/config.go
+++ src/gow-backend/gow/config/config.go
@@ -16,10 +16,13 @@ type Mysql struct {
 	Username string `json:"username" yaml:"username"`
 	Password string `json:"password" yaml:"password"`
 	Database string `json:"database" yaml:"database"`
+	Host     string `json:"host" yaml:"host"`
+	Port     string `json:"port" yaml:"port"`
 }
 
 func (m Mysql) GetURI() string {
-	return fmt.Sprintf("%s:%s@/%s?parseTime=true", m.Username, m.Password, m.Database)
+	fmt.Println("GetURI PORT:", m.Port)
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", m.Username, m.Password, m.Host, m.Port, m.Database)
 }
 
 func SetupConfig() (Config, error) {
@@ -28,12 +31,29 @@ func SetupConfig() (Config, error) {
 	if os.Getenv("ENV") != "" {
 		environment = os.Getenv("ENV")
 	}
-
 	configFile, err := ioutil.ReadFile(fmt.Sprintf("../configs/%s.yml", environment))
 	if err != nil {
 		fmt.Printf("cannot read config file %s", err)
 		return config, err
 	}
 	err = yaml.Unmarshal(configFile, &config)
+	if os.Getenv("PORT") != "" {
+		config.Port = os.Getenv("PORT")
+	}
+	if os.Getenv("MYSQL_USERNAME") != "" {
+		config.Mysql.Username = os.Getenv("MYSQL_USERNAME")
+	}
+	if os.Getenv("MYSQL_PASSWORD") != "" {
+		config.Mysql.Password = os.Getenv("MYSQL_PASSWORD")
+	}
+	if os.Getenv("MYSQL_DATABASE") != "" {
+		config.Mysql.Database = os.Getenv("MYSQL_DATABASE")
+	}
+	if os.Getenv("MYSQL_HOST") != "" {
+		config.Mysql.Host = os.Getenv("MYSQL_HOST")
+	}
+	if os.Getenv("MYSQL_PORT") != "" {
+		config.Mysql.Port = os.Getenv("MYSQL_PORT")
+	}
 	return config, err
 }
