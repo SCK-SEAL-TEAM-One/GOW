818089ba35741b700f892cf1efa280ff461fbd86
ployploy <chonnikan.t@kkumail.com>
2018-09-07 17:12:50 +0700

    [Created]: ฟังก์ชั่น CalculateVat สำหรับคำนวณภาษี

--- src/gow-backend/gow/service/quotation.go
+++ src/gow-backend/gow/service/quotation.go
@@ -9,6 +9,8 @@ type QuotationServiceMySQL struct {
 	QuotationRepository repository.QuotationRepository
 }
 
+const InterestRate = 100
+
 func (quotationServiceMySQL QuotationServiceMySQL) CreateQuotation(model.QuotationForm) (model.QuotationInfo, error) {
 	return model.QuotationInfo{}, nil
 }
@@ -21,4 +23,9 @@ func CalculatePrice(amount int, pricePerUnit float64) float64 {
 func CalculateDiscount(price, discount float64) float64 {
 	priceAfterDiscount := price - discount
 	return priceAfterDiscount
+}
+
+func CalculateVat(price, vatRate float64) float64 {
+	vat := (price * vatRate) / InterestRate
+	return vat
 }
