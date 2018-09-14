f7687164aaa18140d0e77cac65373bf8d44f4f73
smalldog124 <panumars124@gmail.com>
2018-09-14 14:45:05 +0700

    [Edited]: refactor ใช้ companyRepository แทน comRepository

--- src/gow-backend/gow/repository/company.go
+++ src/gow-backend/gow/repository/company.go
@@ -15,13 +15,13 @@ type CompanyRepositoryMySQL struct {
 	DBConnection *sql.DB
 }
 
-func (comrepo CompanyRepositoryMySQL) Insert(newCompany model.NewCompany) (bool, error) {
+func (companyRepository CompanyRepositoryMySQL) Insert(newCompany model.NewCompany) (bool, error) {
 	company := newCompany.ToCompanyModel()
 	statement := `INSERT INTO company 
 	(company_name_th,company_name_en,company_branch_th,company_branch_en,company_address_th,company_address_en,
 	company_taxid,company_phonenumber,created_time,updated_time)
 	VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )`
-	statementInsert, err := comrepo.DBConnection.Prepare(statement)
+	statementInsert, err := companyRepository.DBConnection.Prepare(statement)
 	if err != nil {
 		return false, err
 	}
@@ -34,10 +34,10 @@ func (comrepo CompanyRepositoryMySQL) Insert(newCompany model.NewCompany) (bool,
 
 }
 
-func (comrepo CompanyRepositoryMySQL) GetByTaxID(companyTaxID string) (model.CompanyInfo, error) {
+func (companyRepository CompanyRepositoryMySQL) GetByTaxID(companyTaxID string) (model.CompanyInfo, error) {
 	var company model.Company
 	statementQuery := `SELECT * FROM company WHERE company_taxid=?`
-	row := comrepo.DBConnection.QueryRow(statementQuery, companyTaxID)
+	row := companyRepository.DBConnection.QueryRow(statementQuery, companyTaxID)
 	err := row.Scan(
 		&company.ID,
 		&company.NameTH,
@@ -57,10 +57,10 @@ func (comrepo CompanyRepositoryMySQL) GetByTaxID(companyTaxID string) (model.Com
 	return company.ToCompanyInfo(), nil
 }
 
-func (comrepo CompanyRepositoryMySQL) GetAll() ([]model.CompanyInfo, error) {
+func (companyRepository CompanyRepositoryMySQL) GetAll() ([]model.CompanyInfo, error) {
 	var companyInfo []model.CompanyInfo
 	statementQuery := `SELECT * FROM company`
-	rows, err := comrepo.DBConnection.Query(statementQuery)
+	rows, err := companyRepository.DBConnection.Query(statementQuery)
 	if err != nil {
 		return companyInfo, err
 	}
@@ -82,4 +82,8 @@ func (comrepo CompanyRepositoryMySQL) GetAll() ([]model.CompanyInfo, error) {
 		companyInfo = append(companyInfo, company.ToCompanyInfo())
 	}
 	return companyInfo, nil
+}
+
+func (companyRepository CompanyRepositoryMySQL) GetByID(int) (model.CompanyInfo, error) {
+	return model.CompanyInfo{}, nil
 }
