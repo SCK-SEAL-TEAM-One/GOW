380fc3a66e29f6e462949a3180f4f3b51d73c7c6
smalldog124 <panumars124@gmail.com>
2018-09-14 10:29:56 +0700

    [Added]:เพิ่มมคำสั่งสร้างตาราง quotation

--- src/gow-backend/sql/gow.sql
+++ src/gow-backend/sql/gow.sql
@@ -26,3 +26,27 @@ CREATE TABLE IF NOT EXISTS company (
     created_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_time DATETIME ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS quotation (
+    quotation_id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    quotation_number VARCHAR(13),
+    contact_name VARCHAR(512),
+    contact_email VARCHAR(512),
+    contact_phoneNumber VARCHAR(13),
+	total_price VARCHAR(13),
+    discount_price VARCHAR(13),
+    price_after_discount VARCHAR(13),
+    vat VARCHAR(13),
+    net_total_price VARCHAR(13),
+    total_price_thai VARCHAR(13),
+	project_name VARCHAR(13),
+    quotation_date VARCHAR(512),
+    vat_rate VARCHAR(512),
+    vat_included VARCHAR(512),
+    customer_id INT(3) UNSIGNED ,
+    company_id INT(3) UNSIGNED ,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (company_id) REFERENCES company(company_id),
+    created_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_time DATETIME ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
