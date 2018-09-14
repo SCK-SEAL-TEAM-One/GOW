29bf481ee8d3f0f02b2cc21f574e48c73e1f248f
smalldog124 <panumars124@gmail.com>
2018-09-14 11:33:44 +0700

    [Edited]: เพิ่ม return ไม่ให้เวลา error แล้ว ทำงานต่อใน api quotation

--- src/gow-backend/gow/api/quatation.go
+++ src/gow-backend/gow/api/quatation.go
@@ -17,10 +17,12 @@ func (quotationApi QuotationAPI) CreateQuotationHandler(context *gin.Context) {
 	err := context.Bind(&quotationForm)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, err)
+		return
 	}
 	quotationInfo, err := quotationApi.QuotationService.CreateQuotation(quotationForm)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, err)
+		return
 	}
 	context.JSON(200, quotationInfo)
 }
