90e9c0e45bcfcc0f774da008880b2976865d9532
smalldog124 <panumars124@gmail.com>
2018-09-18 10:04:09 +0700

    [Edited]:แก้ไข QuotationNumber ที่ service/mock_test.go

--- src/gow-backend/gow/service/mock_test.go
+++ src/gow-backend/gow/service/mock_test.go
@@ -87,7 +87,7 @@ func (mock MockQuotationRepository) InsertQuotation(model.QuotationForm, model.P
 func (mock MockQuotationRepository) GetQuotationByID(int64) (model.Quotation, error) {
 	return model.Quotation{
 		QuotationID:        1,
-		QuotationNumber:    "QT201809-000001",
+		QuotationNumber:    "QT256104-101002",
 		ContactName:        "Nopparat Slisatkorn",
 		ContactEmail:       "nopparat.slisatkorn@krungsri.com",
 		ContactPhoneNumber: "",
@@ -98,7 +98,7 @@ func (mock MockQuotationRepository) GetQuotationByID(int64) (model.Quotation, er
 func (mock MockQuotationRepository) GetByQuotationNumber(string) (model.Quotation, error) {
 	return model.Quotation{
 		QuotationID:        1,
-		QuotationNumber:    "QT201809-000001",
+		QuotationNumber:    "QT256104-101002",
 		TotalPrice:         "100,000.00",
 		Discount:           "0.00",
 		PriceAfterDiscount: "100,000.00",
