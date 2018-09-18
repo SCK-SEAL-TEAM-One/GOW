2520d9fc2ec422dce2a107fb8dffee386507106f
smalldog124 <panumars124@gmail.com>
2018-09-18 10:06:45 +0700

    [Added]:เพิ่มฟังก์ชัน GetByQuotationNumber ที่ repository/quotation

---
+++ src/gow-backend/gow/repository/quotation_test.go
@@ -0,0 +1,89 @@
+package repository_test
+
+import (
+	"database/sql"
+	"gow/model"
+	. "gow/repository"
+	"testing"
+)
+
+func Test_InsertQuotation_Input_QuotationForm_And_Payment_And_QuotationNumber_And_VatRate_Should_Be_QuotationID_1(t *testing.T) {
+	url := "root:sckshuhari@tcp(localhost:3306)/GOW?parseTime=true"
+	db, err := sql.Open("mysql", url)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	expectedQuotationId := 1
+	quotationForm := model.QuotationForm{
+		CompanyTaxID:  "0105561001221",
+		CustomerTaxID: "0105537133562",
+		Contact: model.Contact{
+			Name:        "Nopparat Slisatkorn",
+			Email:       "nopparat.slisatkorn@krungsri.com",
+			PhoneNumber: "",
+		},
+		ProjectName: "โครงการ 3 Days Software Testing in Action Workshop",
+		IncludeVAT:  false,
+	}
+	payment := model.Payment{
+		TotalPrice:         "100,000.00",
+		Discount:           "0.00",
+		PriceAfterDiscount: "100,000.00",
+		VAT:                "7,000.00",
+		NetTotalPrice:      "107,000.00",
+		TotalPriceThai:     "หนึ่งแสนเจ็ดพันบาทถ้วน",
+	}
+	vatRate := 7.00
+	quotationNumber := "QT256104-101002"
+
+	quotationRepository := QuotationRepositoryMySQL{
+		DBConnection: db,
+	}
+
+	quotationID, err := quotationRepository.InsertQuotation(quotationForm, payment, quotationNumber, vatRate)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if int64(expectedQuotationId) != quotationID {
+		t.Errorf("expect %d but it got %d", expectedQuotationId, quotationID)
+	}
+}
+
+func Test_GetQuotationByQuotationNumber_Input_QuotationNumber_QT256104_101002_Should_Be_QuotationNmber_QT256104_101002(t *testing.T) {
+	url := "root:sckshuhari@tcp(localhost:3306)/GOW?parseTime=true"
+	db, err := sql.Open("mysql", url)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	expectedQuotation := model.Quotation{
+		QuotationID:     1,
+		QuotationNumber: "QT256104-101002",
+		CustomerTaxID:   "0105537133562",
+		CompanyTaxID:    "0105561001221",
+	}
+
+	quotationNumber := "QT256104-101002"
+	quotationRepository := QuotationRepositoryMySQL{
+		DBConnection: db,
+	}
+
+	quotation, _ := quotationRepository.GetByQuotationNumber(quotationNumber)
+
+	if expectedQuotation.QuotationID != quotation.QuotationID {
+		t.Errorf("expect %v \nbut it got\n %v", expectedQuotation.QuotationID, quotation.QuotationID)
+	}
+}
